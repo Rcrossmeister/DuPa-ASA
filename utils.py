25ad9b6b98409f241a068733b48e56d8cf42fe01
IRis0550 <107255817+IRis0550@users.noreply.github.com>
2023-11-29 12:24:10 +0000

    这是我现在用的utils

--- utils.py
+++ utils.py
@@ -8,25 +8,34 @@ import time
 from datetime import timedelta
 from collections import Counter
 import torch
-from transformers import BertTokenizer, BertModel
+from transformers import XLNetTokenizer, XLNetModel
+from transformers import BertConfig, BertModel, BertTokenizer
+# xlnettokenizer = XLNetTokenizer.from_pretrained("/home/yjy/xlnet-large-cased")
+# xlnetmodel = XLNetModel.from_pretrained("/home/yjy/xlnet-large-cased")
+bert_model = BertModel.from_pretrained("/home/yjy/DuPa-ASA/bert-base-uncased")
+bert_tokenizer = BertTokenizer.from_pretrained("/home/yjy/DuPa-ASA/bert-base-uncased")
+
+
+
 
 MAX_VOCAB_SIZE = 10000  # 词表长度限制 52对于10000适用
 UNK, PAD = '<UNK>', '<PAD>'  # 未知字，padding符号
 
 
+
 class Config:
     def __init__(self):
-        self.train_path = "/Users/iris/Desktop/DuPa-ASA/data/IMDB/train.txt"  # 修改为你的训练数据路径
-        self.dev_path = "/Users/iris/Desktop/DuPa-ASA/data/IMDB/dev.txt"  # 修改为你的验证数据路径
-        self.test_path = "/Users/iris/Desktop/DuPa-ASA/data/IMDB/test.txt"  # 修改为你的测试数据路径
+        self.train_path = "/home/yjy/DuPa-ASA/data/IMDB/data/train.txt"  # 修改为你的训练数据路径
+        self.dev_path = "/home/yjy/DuPa-ASA/data/IMDB/data/dev.txt"  # 修改为你的验证数据路径
+        self.test_path = "/home/yjy/DuPa-ASA/data/IMDB/data/test.txt"  # 修改为你的测试数据路径
         self.pad_size = 512  # 根据需求设置句子的最大长度
-        self.vocab_path = "/Users/iris/Downloads/bert-base-uncased/vocab.pkl"  # 词表路径
-        self.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')  # 设备
+        self.vocab_path = "/home/yjy/DuPa-ASA/bert-base-uncased/vocab.txt"  # 词表路径
+        self.device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')  # 设备
 
 
 def build_dataset(config, use_word):
     if use_word:
-        tokenizer = BertTokenizer.from_pretrained("/Users/iris/Downloads/bert-base-uncased")  # 使用BERT的Tokenizer
+        tokenizer = bert_tokenizer  # 使用BERT的Tokenizer
     else:
         tokenizer = lambda x: [y for y in x]  # char-level
 
@@ -120,60 +129,61 @@ def get_time_dif(start_time):
 
 if __name__ == "__main__":
     '''提取预训练词向量'''
-    model_bert = BertModel.from_pretrained("/Users/iris/Downloads/bert-base-uncased")
-    model_bert.eval()
-    tokenizer_bert = BertTokenizer.from_pretrained("/Users/iris/Downloads/bert-base-uncased")
-
-    train_dir = "/Users/iris/Desktop/DuPa-ASA/data/IMDB/train.txt"
-    vocab_dir = "/Users/iris/Downloads/bert-base-uncased/vocab.txt"
-    filename_trimmed_dir = "/Users/iris/Desktop/DuPa-ASA/data/IMDB/embeddings.npz"
-
-    if os.path.exists(vocab_dir):
-        word_to_id = tokenizer_bert.get_vocab()
-    else:
-        print(False)
-        #tokenizer = lambda x: tokenizer.encode(x, add_special_tokens=False)
-        #word_to_id = build_vocab(train_dir, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
-        #pkl.dump(word_to_id, open(vocab_dir, 'wb'))
-
-    max_sequence_length = 512  # 指定最大的序列长度
-
-    embeddings = []
-
-    with open(train_dir, "r", encoding='UTF-8') as f:
-        for i, line in tqdm(enumerate(f.readlines()), total=len(f.readlines()), desc="Processing"):
-            lin = line.strip()
-            if not lin:
-                continue
-            content, label = lin.split('\t')
-
-            raw_token = tokenizer_bert.tokenize(content)
-
-            # 截断或缩短 raw_token 以适应最大序列长度
-            if len(raw_token) > max_sequence_length:
-                raw_token = raw_token[:max_sequence_length]
-
-            with torch.no_grad():
-                input_ids = tokenizer_bert.convert_tokens_to_ids(raw_token)
-                # 确保输入长度为 max_sequence_length
-                if len(input_ids) < max_sequence_length:
-                    input_ids += [tokenizer_bert.pad_token_id] * (max_sequence_length - len(input_ids))
-                else:
-                    input_ids = input_ids[:max_sequence_length]
-
-                    # 创建 attention_mask
-                attention_mask = [1] * len(input_ids)
-                with torch.no_grad():
-                    output = model_bert(torch.tensor(input_ids).unsqueeze(0),
-                                        attention_mask=torch.tensor(attention_mask).unsqueeze(0))[0].mean(
-                        dim=1).squeeze()
-
-            embeddings.append((output, int(label)))
-
-    f.close()
-
-    embeddings = [e[0].numpy() for e in embeddings]
-    embeddings = np.array(embeddings, dtype='float32')
-    np.savez_compressed(filename_trimmed_dir, embeddings=embeddings)
+    # # 加载BERT模型
+    # xlnettokenizer = XLNetTokenizer.from_pretrained("/home/yjy/enter/xlnet-base-uncased")
+    # xlnetmodel = XLNetModel.from_pretrained("/home/yjy/enter/xlnet-base-uncased")
+    #
+    #
+    # train_dir = "/home/yjy/DuPa-ASA-main/data/IMDB/train.txt"
+    # vocab_dir = "/home/yjy/DuPa-ASA-main/data/IMDB/vocab.txt"
+    # filename_trimmed_dir = "/home/yjy/DuPa-ASA-main/data/IMDB/embeddings.npz"
+    #
+    # if os.path.exists(vocab_dir):
+    #     word_to_id = xlnettokenizer.get_vocab()
+    # else:
+    #     print(False)
+    #     #tokenizer = lambda x: tokenizer.encode(x, add_special_tokens=False)
+    #     #word_to_id = build_vocab(train_dir, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
+    #     #pkl.dump(word_to_id, open(vocab_dir, 'wb'))
+    #
+    # max_sequence_length = 512  # 指定最大的序列长度
+    #
+    # embeddings = []
+    #
+    # with open(train_dir, "r", encoding='UTF-8') as f:
+    #     for i, line in tqdm(enumerate(f.readlines()), total=len(f.readlines()), desc="Processing"):
+    #         lin = line.strip()
+    #         if not lin:
+    #             continue
+    #         content, label = lin.split('\t')
+    #
+    #         raw_token = xlnettokenizer.tokenize(content)
+    #
+    #         # 截断或缩短 raw_token 以适应最大序列长度
+    #         if len(raw_token) > max_sequence_length:
+    #             raw_token = raw_token[:max_sequence_length]
+    #
+    #         with torch.no_grad():
+    #             input_ids = xlnettokenizer.convert_tokens_to_ids(raw_token)
+    #             # 确保输入长度为 max_sequence_length
+    #             if len(input_ids) < max_sequence_length:
+    #                 input_ids += [xlnettokenizer.pad_token_id] * (max_sequence_length - len(input_ids))
+    #             else:
+    #                 input_ids = input_ids[:max_sequence_length]
+    #
+    #                 # 创建 attention_mask
+    #             attention_mask = [1] * len(input_ids)
+    #             with torch.no_grad():
+    #                 output = xlnetmodel(torch.tensor(input_ids).unsqueeze(0),
+    #                                     attention_mask=torch.tensor(attention_mask).unsqueeze(0))[0].mean(
+    #                     dim=1).squeeze()
+    #
+    #         embeddings.append((output, int(label)))
+    #
+    # f.close()
+    #
+    # embeddings = [e[0].numpy() for e in embeddings]
+    # embeddings = np.array(embeddings, dtype='float32')
+    # np.savez_compressed(filename_trimmed_dir, embeddings=embeddings)
 
     print('1')
